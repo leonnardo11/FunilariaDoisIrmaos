44fe048595cf8564e220f32bc96297fd7a85f414
Leonardo Rodrigues <ileonardogtl@gmail.com>
2021-11-18 16:52:30 -0300

    Cadastro Finalizado
    Faltando Recuperar Senha

--- Web/src/pages/Home/Home.jsx
+++ Web/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 import axios from "axios";
+import { userAuthentication } from '../../services/Auth';
 
 // Styles
 import "../../assets/styles/reset.css";
@@ -18,149 +19,121 @@ import close from '../../assets/images/modals/modal-close-icon.svg';
 
 
 export default class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      exemplo : '',
+    constructor(props) {
+        super(props);
+        this.state = {
+            vehicleList: [],
+            isModalOpen: false
+        };
+    }
 
-      isModalOpen : false
+    getUserVehicle = (user) => {
+        axios('http://44.195.209.235/api/Vehicles/User/b6c986fb-c496-41c1-a2b9-5a4bb900c160' , {
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('user-token')
+            }
+        })
+            .then(resposta => {
+                if (resposta.status === 200) {
+                    this.setState({ vehicleList: resposta.data })
+                    console.log(this.state.vehicleList)
+                }
+            })
+            .catch(erro => console.log(erro));
     };
-  }
 
-  cancelaModal = () => {
-    this.setState({ isModalOpen : false })
-  }
+    // Chama as funções assim que a tela é renderizada
+    componentDidMount() {
+        this.getUserVehicle();
+        document.title = "Meus Veículos"
+    };
 
-  render() {
-    return (
-        <>
-            <Sidebar>
-                <div className="home-title">
-                  <h1>Meus Veículos</h1>
-                </div>
-                <div className="home-btn">
-                    <button onClick={() => this.setState({isModalOpen : true})}>Novo Veículo</button>
-                </div>
-                
-                {/* Card dos carros */}
-                <div className="home-card-background">
-                    {/* Vocês vão colocar essa 'div' (Link) dentro do 'map' para conseguir listar */}
-                    <Link to="/budgets" className="home-content-background">
-                        <div className="home-content-car-image">
-                            <img src={car} alt="Imagem de um carro vermelho" draggable="false" />
-                        </div>
+    cancelaModal = () => {
+        this.setState({ isModalOpen: false })
+    }
 
-                        <div className="home-content-text">
-                            <h1>Chevrolet Onix</h1>
-                            <p>Placa: ABC-1234</p>
-                            <p>Cor: Vermelho</p>
-                        </div>
+    render() {
+        return (
+            <>
+                <Sidebar>
+                    <div className="home-title">
+                        <h1>Meus Veículos</h1>
+                    </div>
+                    <div className="home-btn">
+                        <button onClick={() => this.setState({ isModalOpen: true })}>Novo Veículo</button>
+                    </div>
+                    <div className="home-card-background">
+                        {
+                            this.state.vehicleList.map(vehicle => {
+                                return (
+                                    <Link className="home-content-background">
+                                        <div className="home-content-car-image">
+                                            <img src={car} alt="Imagem de um carro vermelho" draggable="false" />
+                                        </div>
 
-                        <div className="home-content-btn">
-                            <p>Visualizar Orçamentos</p>
-                        </div>
-                    </Link>
-                    
-                    {/* Exemplo (apagar!) */}
-                    <Link className="home-content-background">
-                        <div className="home-content-car-image">
-                            <img src={car} alt="Imagem de um carro vermelho" draggable="false" />
-                        </div>
+                                        <div className="home-content-text">
+                                        <h1>{vehicle.brandName} {vehicle.modelName}</h1>
+                                            <p>Placa: {vehicle.licensePlate}</p>
+                                            <p>Cor: {vehicle.color}</p>
+                                        </div>
 
-                        <div className="home-content-text">
-                            <h1>Chevrolet Onix</h1>
-                            <p>Placa: ABC-1234</p>
-                            <p>Cor: Vermelho</p>
-                        </div>
-
-                        <div className="home-content-btn">
-                            <p>Visualizar Orçamentos</p>
-                        </div>
-                    </Link>
-
-                    {/* Exemplo (apagar!) */}
-                    <Link className="home-content-background">
-                        <div className="home-content-car-image">
-                            <img src={car} alt="Imagem de um carro vermelho" draggable="false" />
-                        </div>
-
-                        <div className="home-content-text">
-                            <h1>Chevrolet Onix</h1>
-                            <p>Placa: ABC-1234</p>
-                            <p>Cor: Vermelho</p>
-                        </div>
-
-                        <div className="home-content-btn">
-                            <p>Visualizar Orçamentos</p>
-                        </div>
-                    </Link>
-
-                    {/* Exemplo (apagar!) */}
-                    <Link className="home-content-background">
-                        <div className="home-content-car-image">
-                            <img src={car} alt="Imagem de um carro vermelho" draggable="false" />
-                        </div>
-
-                        <div className="home-content-text">
-                            <h1>Chevrolet Onix</h1>
-                            <p>Placa: ABC-1234</p>
-                            <p>Cor: Vermelho</p>
-                        </div>
-
-                        <div className="home-content-btn">
-                            <p>Visualizar Orçamentos</p>
-                        </div>
-                    </Link>
-                </div>
-            </Sidebar>
+                                        <div className="home-content-btn">
+                                            <p>Visualizar Orçamentos</p>
+                                        </div>
+                                    </Link>
+                                );
+                            })
+                        }
+                    </div>
+                </Sidebar>
 
 
 
-            {/* Modal */}
-            <Modal isOpen={this.state.isModalOpen}>
-                <div className="modal-overlay">
-                    <div className="modal" id="modal" onClick={() => document.getElementById('modal-card').click() ? '' : this.cancelaModal()}></div>
-                    <div className="modal-card-background" id="modal-card">
-                        <div className="modal-card-close">
-                            <img src={close} alt="Ícone para fechar o modal" draggable="false" onClick={() => this.cancelaModal()} />
-                        </div>
+                {/* Modal */}
+                <Modal isOpen={this.state.isModalOpen}>
+                    <div className="modal-overlay">
+                        <div className="modal" id="modal" onClick={() => document.getElementById('modal-card').click() ? '' : this.cancelaModal()}></div>
+                        <div className="modal-card-background" id="modal-card">
+                            <div className="modal-card-close">
+                                <img src={close} alt="Ícone para fechar o modal" draggable="false" onClick={() => this.cancelaModal()} />
+                            </div>
 
-                        <div className="modal-vehicle-card-form-background">
-                            <div className="modal-vehicle-card-form">
-                                <div className="modal-vehicle-card-form-text">
-                                    <h1>Adicionar um novo veículo</h1>
-                                    <p>Adicione as informações do seu Veículo</p>
-                                </div>
-
-                                <div className="modal-vehicle-card-form-input-background">
-                                    <div className="modal-vehicle-card-form-input">
-                                        <input type="text" placeholder="Insira o Modelo de seu Veículo" />
+                            <div className="modal-vehicle-card-form-background">
+                                <div className="modal-vehicle-card-form">
+                                    <div className="modal-vehicle-card-form-text">
+                                        <h1>Adicionar um novo veículo</h1>
+                                        <p>Adicione as informações do seu Veículo</p>
                                     </div>
 
-                                    <div className="modal-vehicle-card-form-input">
-                                        <input type="text" placeholder="Marca" />
-                                    </div>
+                                    <div className="modal-vehicle-card-form-input-background">
+                                        <div className="modal-vehicle-card-form-input">
+                                            <input type="text" placeholder="Insira o Modelo de seu Veículo" />
+                                        </div>
 
-                                    <div className="modal-vehicle-card-form-input">
-                                        <input type="year" placeholder="Ano" />
-                                    </div>
+                                        <div className="modal-vehicle-card-form-input">
+                                            <input type="text" placeholder="Marca" />
+                                        </div>
 
-                                    <div className="modal-vehicle-card-form-input">
-                                        <input type="text" placeholder="Cor" />
-                                    </div>
+                                        <div className="modal-vehicle-card-form-input">
+                                            <input type="year" placeholder="Ano" />
+                                        </div>
 
-                                    <div className="modal-vehicle-card-form-input">
-                                        <input type="text" placeholder="Placa" />
-                                    </div>
+                                        <div className="modal-vehicle-card-form-input">
+                                            <input type="text" placeholder="Cor" />
+                                        </div>
 
-                                    <button>Adicionar Veículo</button>
+                                        <div className="modal-vehicle-card-form-input">
+                                            <input type="text" placeholder="Placa" />
+                                        </div>
+
+                                        <button>Adicionar Veículo</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </Modal>
-        </>
-    );
-  }
+                </Modal>
+            </>
+        );
+    }
 }
--- Web/src/pages/Register/Register.jsx
+++ Web/src/pages/Register/Register.jsx
@@ -2,6 +2,8 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 import axios from "axios";
+import swal from 'sweetalert';
+
 
 // Styles
 import "../../assets/styles/reset.css";
@@ -11,70 +13,112 @@ import "../../assets/styles/pages/register.css";
 import banner from '../../assets/images/pages/register-banner.svg';
 
 export default class Register extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      exemplo : ''
+    constructor(props) {
+        super(props);
+        this.state = {
+            exemplo: ''
+        };
+    }
+
+    cadUser = (event) => {
+        event.preventDefault();
+        this.setState({ isLoading: true });
+
+        let user = {
+            Username: this.state.Username,
+            email: this.state.Email,
+            password: this.state.Password,
+            phoneNumber: this.state.PhoneNumber
+        };
+        axios.post('http://44.195.209.235/api/Users', user, {
+
+        })
+
+            .then(resposta => {
+                if (resposta.status === 201) {
+                    this.setState({ isLoading: false });
+                    swal("Sucesso!", "Usuário Cadastrado com Sucesso", "success");
+
+                }
+            })
+            .catch(erro => {
+                swal("Erro!", "Ocorreu um Erro " + {erro} , "error");
+                this.setState({ isLoading: false });
+            })
     };
-  }
 
-  render() {
-    return (
-        <>
-            <div className="register-background">
-                {/* Área do formulário (lado esquerdo) */}
-                <div className="register-form-background">
-                    <div className="register-form-content">
-                        <div className="register-form-text">
-                            <h1>Criar uma nova conta</h1>
-                            <p>Crie uma conta para que você possa acompanhar os nossos Serviços.</p>
+    atualizaStateCampo = (campo) => {
+        this.setState({ [campo.target.name] : campo.target.value })
+    };
+
+    render() {
+        return (
+            <>
+                <div className="register-background">
+                    {/* Área do formulário (lado esquerdo) */}
+                    <div className="register-form-background">
+                        <div className="register-form-content">
+                            <div className="register-form-text">
+                                <h1>Criar uma nova conta</h1>
+                                <p>Crie uma conta para que você possa acompanhar os nossos Serviços.</p>
+                            </div>
+
+                            <form onSubmit={this.cadUser}>
+                                <div className="register-form-main">
+                                    <div className="register-form-input">
+                                        <input type="text" placeholder="Digite o seu nome" 
+                                        name="Username"
+                                        value={this.state.username}
+                                        onChange={this.atualizaStateCampo} />
+                                    </div>
+
+                                    <div className="register-form-input">
+                                        <input type="email" placeholder="Digite o seu e-mail" 
+                                        value={this.state.email}
+                                        name="Email"
+                                        onChange={this.atualizaStateCampo}/>
+                                    </div>
+
+                                    <div className="register-form-input-separated">
+                                        <div className="register-form-input">
+                                            <input type="password" placeholder="Digite uma senha"
+                                            name="Password"
+                                            value={this.state.password}
+                                            onChange={this.atualizaStateCampo}/>
+                                        </div>
+
+                                      
+                                    </div>
+
+                                    <div className="register-form-input-separated">
+                                        <div className="register-form-input">
+                                            <input type="tel" placeholder="Digite o seu telefone" 
+                                            value={this.state.phoneNumber}
+                                            name="PhoneNumber"
+                                            onChange={this.atualizaStateCampo}
+                                            />
+                                        </div>
+
+                                        
+                                    </div>
+
+                                    <div className="register-form-btns">
+                                        <button>Criar conta</button>
+                                        <Link to="/">Já tenho uma conta!</Link>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
+                    </div>
 
-                        <div className="register-form-main">
-                            <div className="register-form-input">
-                                <input type="text" placeholder="Digite o seu nome" />
-                            </div>
-
-                            <div className="register-form-input">
-                                <input type="text" placeholder="Digite o seu e-mail" />
-                            </div>
-
-                            <div className="register-form-input-separated">
-                                <div className="register-form-input">
-                                    <input type="password" placeholder="Digite uma senha" />
-                                </div>
-
-                                <div className="register-form-input">
-                                    <input type="password" placeholder="Repita a senha" />
-                                </div>
-                            </div>
-
-                            <div className="register-form-input-separated">
-                                <div className="register-form-input">
-                                    <input type="tel" placeholder="Digite o seu telefone" />
-                                </div>
-
-                                <div className="register-form-input">
-                                    <input type="tel" placeholder="Digite o seu celular" />
-                                </div>
-                            </div>
-
-                            <div className="register-form-btns">
-                                <button>Criar conta</button>
-                                <Link to="/">Já tenho uma conta!</Link>
-                            </div>
+                    {/* Área do banner (lado direito) */}
+                    <div className="register-banner-background">
+                        <div className="register-banner-content">
+                            <img src={banner} alt="Banner" draggable="false" />
                         </div>
                     </div>
                 </div>
-
-                {/* Área do banner (lado direito) */}
-                <div className="register-banner-background">
-                    <div className="register-banner-content">
-                        <img src={banner} alt="Banner" draggable="false" />
-                    </div>
-                </div>
-            </div>
-        </>
-    );
-  }
+            </>
+        );
+    }
 }
