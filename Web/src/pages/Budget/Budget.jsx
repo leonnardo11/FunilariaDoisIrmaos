73534f74867e44e87339dec3c75e5b5dae83fe22
Leonardo Rodrigues <54442873+leonnardo11@users.noreply.github.com>
2021-12-09 16:25:17 -0300

    Update Budget.jsx

--- Web/src/pages/Budget/Budget.jsx
+++ Web/src/pages/Budget/Budget.jsx
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from "axios";
+import { parseJwt, userAuthentication } from '../../services/Auth';
 
 // Styles
 import '../../assets/styles/reset.css';
@@ -12,7 +13,7 @@ import Sidebar from '../../components/Sidebar';
 
 
 class Budget extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             BudgetList: [],
@@ -20,54 +21,54 @@ class Budget extends Component {
     }
 
     getVehicleBudget = () => {
-        axios('https://54.147.100.207/api/Budgets')
+        axios.get('https://54.147.100.207/api/Budgets/Vehicle/7f99dc6d-4395-4c03-b035-1f4229bc3d60')
             .then(resposta => {
                 if (resposta.status === 200) {
                     this.setState({ BudgetList: resposta.data })
                     console.log(this.state.BudgetList);
+                    console.log(typeof (this.state.BudgetList))
                 }
             })
             .catch(erro => console.log(erro));
     };
 
-    componentDidMount(){
+    componentDidMount() {
         this.getVehicleBudget();
         document.title = "Orçamento do Veículo"
     }
 
     render() {
-        return(
+        return (
             <>
                 <Sidebar>
-                    <div className="budget-header">
-                        <Link to="/home" className="budget-header-back">{"< Meus Veículos"}</Link>
-                        <div className="budget-title">
-                            <h1></h1>
-                        </div>
-                        <div className="budget-placa">
-                            <p>Placa: ABC-1234</p>
-                        </div>
-                    </div>
-                    
-                    <div className="budget-card-background">
-                        <p className="budget-card-background-title">Orçamentos</p>
+                    {
 
-                        <div className="budget-card-content-background">
-                            <Link to="/services" className="budget-content-background">
-                                <div className="budget-content-text">
-                                    <h1>Orçamento #0001</h1>
-                                     <p>Data de Início: 20/09/2021</p>
-                                     <p>Data de Término: ----</p>
+                        <>
+                            <div className="budget-card-background">
+                                <p className="budget-card-background-title">Orçamentos</p>
+
+                                <div className="budget-card-content-background">
+                                    <Link to="/services" className="budget-content-background">
+                                        <div className="budget-content-text">
+                                            <h1>Orçamento</h1>
+                                            <p>Data de Início: {this.state.BudgetList.creationDate}1</p>
+                                            <p>Data de Término: ----</p>
+                                        </div>
+
+                                        <div className="budget-content-btn">
+                                            <p>Status: Em Andamento</p>
+                                            <p className="budget-content-btn-valor">Valor: R$ {this.state.BudgetList.totalValue}</p>
+                                        </div>
+                                    </Link>
                                 </div>
 
-                                <div className="budget-content-btn">
-                                     <p>Status: Em Andamento</p>
-                                     <p className="budget-content-btn-valor">Valor: R$ 3.872,28</p>
-                                </div>
-                            </Link>
-                        </div>
-    
-                    </div>
+                            </div>
+                        </>
+
+                    })
+
+
+                  
                 </Sidebar>
             </>
         )
